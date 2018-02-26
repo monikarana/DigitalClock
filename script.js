c5b116e5f7df6826667c3ee6fdda4340742dc8f8
Rana <monika.rana@intel.com>
2018-02-26 19:19:03 +0530

    increment degrees fro each hand instead of reseting it from zero

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
-function reunTheClock() {
 var date = new Date();
 console.log(date);
 
@@ -18,9 +16,14 @@ let minPosition = ((min * 360/60) +  (sec * 360/60) / 60);
 let secPosition = sec * 360/60;
 
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+function reunTheClock() {
+  hrPosition = hrPosition + (3/360);
+  minPosition = minPosition + (6/60);
+  secPosition = secPosition + 6;
+  
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
 var interval = setInterval(reunTheClock, 1000);
